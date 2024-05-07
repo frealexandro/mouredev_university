177ca11277f003288d6896b2fe56176e76de971c
frealexandro <jhoner300@gmail.com>
2024-05-06 22:00:38 -0500

    tengo un error , sim embargo se entiende mucho mas la extructura de toda la pagina web y ya se agrego el flex que representara el sitebar

--- mouredev_university/mouredev_university.py
+++ mouredev_university/mouredev_university.py
@@ -1,6 +1,9 @@
 import reflex as rx
 import mouredev_university.styles.styles as styles
 from mouredev_university.components.navbar import navbar
+from mouredev_university.components.button import li
+from mouredev_university.views.index import index
+
 #import mouredev_university.utils as utils
 
 
@@ -11,7 +14,14 @@ from mouredev_university.components.navbar import navbar
 
 def index() -> rx.Component:
     return rx.box(  
+        rx.script(src="document.documentElement.lang='es'"),
         navbar(),
+        rx.flex(
+            index(),
+            direction="column",
+            align_items="center",
+            padding_y=styles.Size.BIG.value
+        ),
     )
 
 
